b858a0e33083e060d2c721484d3e0654e9acd11b
GustavoManzoni <167345770+GustavoManzoni@users.noreply.github.com>
2024-10-21 14:38:27 -0300

    plkbcfm,lç~ç,ml bl,df~]ç~,fd~]çnf x

--- TrabalhoHistória/Assets/Scripts/Placa.cs
+++ TrabalhoHistória/Assets/Scripts/Placa.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
-
+using TMPro;
 public class Placa : MonoBehaviour
 {
    
-    public GameObject interactionPrompt, mensagem;
+    public GameObject interactionPrompt, mensagem, mensagem1;
     public KeyCode interactionKey = KeyCode.E;
     public float interactionRange = 2.0f;
     private Transform player;
-    
+  
+    public TMP_Text texto;
     private void Start()
     {
-       
+
         player = GameObject.FindGameObjectWithTag("Player").transform;
         interactionPrompt.SetActive(false);
     }
-
     void Update()
     {
         float distance = Vector2.Distance(transform.position, player.position);
@@ -22,10 +22,12 @@ public class Placa : MonoBehaviour
         if (distance <= interactionRange)
         {
             interactionPrompt.SetActive(true);
-            interactionPrompt.transform.position = transform.position + new Vector3(0, 1.5f, 0); 
+            interactionPrompt.transform.position = transform.position + new Vector3(0, 1.5f, 0);
+           
 
             if (Input.GetKeyDown(interactionKey))
             {
+
                 Interact();
             }
         }
@@ -38,11 +40,32 @@ public class Placa : MonoBehaviour
     
     public void Interact()
     {
-        mensagem.SetActive(true);
+        if(gameObject.tag == "Placa1")
+        {
+
+            mensagem1.SetActive(true);
+
+        }
+        else
+        {
+            mensagem.SetActive(true);
+
+        }
+        
     }
     public void Desativar()
     {
-        mensagem.SetActive(true);
+        if (gameObject.tag == "Placa1")
+        {
+
+            mensagem1.SetActive(false);
+
+        }
+        else
+        {
+            mensagem.SetActive(false);
+
+        }
 
 
     }
